fddbed7fc2e89fa9e52d4090df0135d07f75d432
Hessam Imen <hessam.imen@gmail.com>
2022-03-20 07:17:25 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.getElementById('text').addEventListener('DOMContentLoaded', function(){
+    p.innerHTML = "This is really cool!"
+    console.log('test')
+    
+})
+document.addEventListener('DOMContentLoaded', function(){
+    let text = document.getElementById('text');
+    text.innerHTML = "This is really cool!"
+
+})
